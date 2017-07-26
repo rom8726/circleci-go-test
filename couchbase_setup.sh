c2695e088d51c5b62691b38b8f4d8e6c0d3738d6
Roman Chudov <roman.chudov@gmail.com>
2017-07-26 15:57:57 +0700

    sleep 10 seconds

--- couchbase_setup.sh
+++ couchbase_setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+sleep 10;
 curl -X POST -u Administrator:password -d 'name=test' -d 'ramQuotaMB=100' -d 'authType=none' -d 'proxyPort=11216' http://127.0.0.1:8091/pools/default/buckets
